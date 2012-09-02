11adb5764f2eccdf99f1fc604cebe9952a355a1c
Alexander Schlueter <arleckshunt@googlemail.com>
2012-09-02 21:15:52 +0200

    fix Number_jmodule parsing of 0

--- modules/basicarith/basicarith.cpp
+++ modules/basicarith/basicarith.cpp
@@ -69,7 +69,7 @@ OperatorInterface BASICARITHSHARED_EXPORT Exponentiation_jmodule()
 
 std::unique_ptr<CAS::AbstractArithmetic> BASICARITHSHARED_EXPORT Number_jmodule(const std::string &candidate)
 {
-    if (candidate.size() == 1 && candidate.front() == '0') return nullptr;
+    if (candidate.size() != 1 && candidate.front() == '0') return nullptr;
     else {
         if (candidate.find_first_not_of("0123456789") != std::string::npos) return nullptr;
         else return make_unique<CAS::NumberArith>(CAS::Natural(candidate));
